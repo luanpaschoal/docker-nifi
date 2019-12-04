2c3af078814ad6252495e4596ff54eefd0921200
Edson Marquezani Filho <edsonmarquezani@gmail.com>
2019-12-04 17:08:48 -0300

    Change variable names

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ CLASSPATH=$(ls -1 ${NIFI_HOME}/lib/*.jar | tr '\n' ':')
 CLASSPATH="${CLASSPATH}${CONF_DIR}"
 
 # Override JVM memory settings
-if [ ! -z "${NIFI_JVM_HEAP_INIT}" ]; then
-  JAVA_OPTS="${JAVA_OPTS} -Xms${NIFI_JVM_HEAP_INIT}"
+if [ ! -z "${JVM_HEAP_INIT}" ]; then
+  JAVA_OPTS="${JAVA_OPTS} -Xms${JVM_HEAP_INIT}"
 fi
 
-if [ ! -z "${NIFI_JVM_HEAP_MAX}" ]; then
-  JAVA_OPTS="${JAVA_OPTS} -Xmx${NIFI_JVM_HEAP_MAX}"
+if [ ! -z "${JVM_HEAP_MAX}" ]; then
+  JAVA_OPTS="${JAVA_OPTS} -Xmx${JVM_HEAP_MAX}"
 fi
 
 JAVA_OPTS="${JAVA_OPTS} -Dorg.apache.jasper.compiler.disablejsr199=true -Djavax.security.auth.useSubjectCredsOnly=true -Djava.security.egd=file:/dev/urandom -Dsun.net.http.allowRestrictedHeaders=true -Djava.net.preferIPv4Stack=true -Djava.awt.headless=true -Djava.protocol.handler.pkgs=sun.net.www.protocol -Dnifi.bootstrap.listen.port=41491 -Dorg.apache.nifi.bootstrap.config.log.dir=/opt/nifi/nifi-current/logs"
