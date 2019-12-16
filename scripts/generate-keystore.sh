e60856b1cd38b1e52d5d9ddf5d4294c10f401856
Edson Marquezani Filho <edsonmarquezani@gmail.com>
2019-12-16 11:25:12 -0300

    Update readme

--- scripts/generate-keystore.sh
+++ scripts/generate-keystore.sh
@@ -14,22 +14,29 @@
 
 cert=${1:-/opt/nifi/nifi-current/ssl/cert.pem}
 key=${2:-/opt/nifi/nifi-current/ssl/key.pem}
-out_dir=${3:-/opt/nifi/nifi-current/conf}
+ca=${3:-/opt/nifi/nifi-current/ssl/ca.pem}
+out_dir=${4:-/opt/nifi/nifi-current/conf}
 
 # Password strength doesn't really matter in this case
 PASSWORD="nifinifi"
 
 if [ -f ${cert} -a -f ${key} ]; then
-  openssl pkcs12 -export -in ${cert} -inkey ${key} -passout "pass:${PASSWORD}" -name nifi-key > /tmp/nifi.p12 && \
+  if [ -e ${ca} ]; then
+    cat ${cert} ${ca} > ${out_dir}/cert.pem
+  else
+    echo "CA file not found."
+    cp ${cert} ${out_dir}/cert.pem
+  fi
+  openssl pkcs12 -export -in ${out_dir}/cert.pem -inkey ${key} -passout "pass:${PASSWORD}" -name nifi-key > /tmp/nifi.p12 && \
   keytool -importkeystore -srckeystore /tmp/nifi.p12 -destkeystore ${out_dir}/keystore.jks -noprompt \
     -srcstoretype pkcs12 -alias nifi-key -deststorepass ${PASSWORD} -srcstorepass ${PASSWORD} && \
-  keytool -import -alias nifi-cert -file ${cert} -keystore ${out_dir}/truststore.jks \
+  keytool -importcert -alias nifi-cert -file ${out_dir}/cert.pem -trustcacerts -keystore ${out_dir}/truststore.jks \
     -deststorepass ${PASSWORD} -noprompt && \
   echo -e "\nWritten files: ${out_dir}/keystore.jks and ${out_dir}/truststore.jks with password '${PASSWORD}'."
 else
   echo "Certificates not found. Auto-generating key-pair."
   /opt/nifi/nifi-toolkit-current/bin/tls-toolkit.sh standalone -n 'localhost' -C 'CN=localhost, OU=NIFI' \
-    -o /tmp --keyPassword ${PASSWORD} --clientCertPassword ${PASSWORD} && \
+    -o /tmp --trustStorePassword ${PASSWORD} --keyStorePassword ${PASSWORD} --keyPassword ${PASSWORD} && \
   cp /tmp/localhost/keystore.jks ${out_dir}/keystore.jks && \
   cp /tmp/localhost/truststore.jks ${out_dir}/truststore.jks && \
   echo -e "\nWritten files: ${out_dir}/keystore.jks and ${out_dir}/truststore.jks with password '${PASSWORD}'".
