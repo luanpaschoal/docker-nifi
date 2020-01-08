c31e686d1ef33a7aa05504f11f4544dcb0d0c6de
Edson Marquezani Filho <edsonmarquezani@gmail.com>
2020-01-08 18:53:30 -0300

    Fix entrypoint classpath generation and check for template directory existence

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 CONF_DIR="${NIFI_HOME}/conf"
-CLASSPATH=$(ls -1 ${NIFI_HOME}/lib/*.jar | tr '\n' ':')
+CLASSPATH=$(find ${NIFI_HOME}/lib -type f | tr '\n' ':')
 CLASSPATH="${CLASSPATH}${CONF_DIR}"
 
 # Override JVM memory settings
@@ -16,10 +16,12 @@ fi
 
 JAVA_OPTS="${JAVA_OPTS} -Dorg.apache.jasper.compiler.disablejsr199=true -Djavax.security.auth.useSubjectCredsOnly=true -Djava.security.egd=file:/dev/urandom -Dsun.net.http.allowRestrictedHeaders=true -Djava.net.preferIPv4Stack=true -Djava.awt.headless=true -Djava.protocol.handler.pkgs=sun.net.www.protocol -Dorg.apache.nifi.bootstrap.config.log.dir=/opt/nifi/nifi-current/logs"
 
-for file in $(ls -1 ${CONF_DIR}/templates | grep -E '\.tpl$'); do
-  out_file=${file%%.tpl}
-  gomplate -f ${CONF_DIR}/templates/${file} -o ${CONF_DIR}/${out_file} && echo "Generated config file from template in ${CONF_DIR}/${out_file}"
-done
+if [ -d ${CONF_DIR}/templates ]; then
+  for file in $(ls -1 ${CONF_DIR}/templates | grep -E '\.tpl$'); do
+    out_file=${file%%.tpl}
+    gomplate -f ${CONF_DIR}/templates/${file} -o ${CONF_DIR}/${out_file} && echo "Generated config file from template in ${CONF_DIR}/${out_file}"
+  done
+fi
 
 JAVA_CMD="java -classpath ${CLASSPATH} -Dnifi.properties.file.path=${NIFI_HOME}/conf/nifi.properties ${JAVA_OPTS} org.apache.nifi.NiFi"
 
