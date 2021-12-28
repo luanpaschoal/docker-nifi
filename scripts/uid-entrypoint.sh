683fcae12251bcc3197dfd4cb2130d6106e208bb
luanpaschoal <luanpaschoal@yahoo.com.br>
2021-12-28 10:23:25 -0300

    Delete uid-entrypoint.sh

--- scripts/uid-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-nifi}:x:$(id -u):0:${USER_NAME:-nifi} user:${HOME}:/sbin/nologin" >> /etc/passwd
-  fi
-fi
-
-exec "$@" 
